c90c2261c1f3587633242dd68492b24a743638c4
Bharath M R <150921529+bharath-synup@users.noreply.github.com>
2024-09-23 15:48:05 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,18 @@
 import pytest
-# conftest.py
+
 def pytest_addoption(parser):
-    parser.addoption("--client_name", action="store", default="Default Client Name", help="Client Name to use in the test")
+    parser.addoption("--client_name", action="store", default="default_client_name")
+    parser.addoption("--logo_url", action="store", default="default_logo_url")
+    parser.addoption("--base_url", action="store", default="https://example.com")
 
 @pytest.fixture
 def client_name(request):
     return request.config.getoption("--client_name")
+
+@pytest.fixture
+def logo_url(request):
+    return request.config.getoption("--logo_url")
+
+@pytest.fixture
+def base_url(request):
+    return request.config.getoption("--base_url")
