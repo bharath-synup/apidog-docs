3a55030d72110d9c15b545417fa1226586b2ecf0
Bharath M R <150921529+bharath-synup@users.noreply.github.com>
2024-11-18 12:31:57 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ def pytest_addoption(parser):
     parser.addoption("--client_name", action="store", default="default_client_name")
     parser.addoption("--client_logo_url", action="store", default="default_logo_url")
     parser.addoption("--client_base_url", action="store", default="https://example.com")
+    parser.addoption("--client_api_docs_url", action="store", default="https://example.com/api-docs")
 
 @pytest.fixture
 def client_name(request):
@@ -16,3 +17,7 @@ def client_logo_url(request):
 @pytest.fixture
 def client_base_url(request):
     return request.config.getoption("--client_base_url")
+
+@pytest.fixture
+def client_api_docs_url(request):
+    return request.config.getoption("--client_api_docs_url")
